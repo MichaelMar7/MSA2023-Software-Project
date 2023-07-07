5f5fab36532c8f693862485f929f38f3a6c5dd54
Michael Mar <michaelmar719@gmail.com>
2023-07-07 18:43:23 +1200

    created sql db server on azure

---
+++ back-end/Back-End/Contexts/UserContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Back_End.Contexts
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- back-end/Back-End/Program.cs
+++ back-end/Back-End/Program.cs
@@ -1,11 +1,41 @@
+using Back_End.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
+//// Retrieve the configuration from appsettings.json
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+// Add services to the container.
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<UserContext>(options =>
+{
+    options.UseSqlServer(configuration.GetConnectionString("sqlDatabase"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Supercell Accounts API", Version = "v1" });
+});
+builder.Services.AddHttpClient();
+builder.Services.AddCors(options => 
+{ 
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy => {
+            policy.WithOrigins("http://localhost:3000" /*Host URL*/)
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -13,7 +43,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(x =>
+    {
+        x.SwaggerEndpoint("/swagger/v1/swagger.json", "Supercell Accounts API");
+    });
 }
 
 app.UseHttpsRedirection();
